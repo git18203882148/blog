830ffdcae07123581b087198c2d2db3613062ffa
wangyulue <1206180097@qq.com>
2020-02-13 16:08:06 +0800

    feat(*):add new article

--- docs/.vuepress/config/nav.js
+++ docs/.vuepress/config/nav.js
@@ -2,5 +2,5 @@ module.exports = [
   { text: 'Code', link: '/code/' },
   { text: 'Read', link: '/read/' },
   { text: 'View', link: '/view/' },
-  { text: 'Think', link: '/think/' },
+  { text: 'Write', link: '/write/' },
 ]
--- docs/.vuepress/config/sidebar.js
+++ docs/.vuepress/config/sidebar.js
@@ -2,5 +2,5 @@ module.exports = {
   '/code/': require('../../code/sidebar'),
   '/read/': require('../../read/sidebar'),
   '/view/': require('../../view/sidebar'),
-  '/think/': require('../../think/sidebar'),
+  '/write/': require('../../write/sidebar'),
 }
